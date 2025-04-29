9a6370d0b8cc46ad358e4c5f31e58880a36dbb26
Patgart <patgart@gmx.de>
2025-04-29 12:08:56 +0200

    Changed ci workflow

--- Tests/Checkpoint.GaiaApi.Tests.ps1
+++ Tests/Checkpoint.GaiaApi.Tests.ps1
@@ -15,26 +15,26 @@ Describe 'Checkpoint.GaiaApi module' {
     }
     Context 'Cmdlet availability' {
         $cmdLets = @(
-            @{cmdLet = 'Connect-GaiaSession';}
-            @{cmdLet = 'Disconnect-GaiaSession';}
+            @{cmdLet = 'Connect-GaiaSession';},
+            @{cmdLet = 'Disconnect-GaiaSession';},
             # Diagnostics
-            @{cmdLet = 'Get-GaiaConnectionsPresets';}
-            @{cmdLet = 'Get-GaiaConnections';}
-            @{cmdLet = 'Get-GaiaTask';}
-            @{cmdLet = 'Set-GaiaOpenTelemetry';}
-            @{cmdLet = 'Get-GaiaOpenTelemetry';}
-            @{cmdLet = 'Get-GaiaStatisticsInfo';}
-            @{cmdLet = 'Get-GaiaStatistics';}
-            @{cmdLet = 'Get-GaiaStatisticsViewInfo';}
+            @{cmdLet = 'Get-GaiaConnectionsPresets';},
+            @{cmdLet = 'Get-GaiaConnections';},
+            @{cmdLet = 'Get-GaiaTask';},
+            @{cmdLet = 'Set-GaiaOpenTelemetry';},
+            @{cmdLet = 'Get-GaiaOpenTelemetry';},
+            @{cmdLet = 'Get-GaiaStatisticsInfo';},
+            @{cmdLet = 'Get-GaiaStatistics';},
+            @{cmdLet = 'Get-GaiaStatisticsViewInfo';},
             # Authentication servers
-            @{cmdLet = 'Get-GaiaRadius';}
-            @{cmdLet = 'Set-GaiaRadius';}
-            @{cmdLet = 'Get-GaiaTacacs';}
-            @{cmdLet = 'Set-GaiaTacacs';}
-            @{cmdLet = 'Get-GaiaAuthenticationOrder';}
-            @{cmdLet = 'Set-GaiaAuthenticationOrder';}
+            @{cmdLet = 'Get-GaiaRadius';},
+            @{cmdLet = 'Set-GaiaRadius';},
+            @{cmdLet = 'Get-GaiaTacacs';},
+            @{cmdLet = 'Set-GaiaTacacs';},
+            @{cmdLet = 'Get-GaiaAuthenticationOrder';},
+            @{cmdLet = 'Set-GaiaAuthenticationOrder';},
             # Allowed Clients
-            @{cmdLet = 'Get-GaiaAllowedClients';}
+            @{cmdLet = 'Get-GaiaAllowedClients';},
             @{cmdLet = 'Set-GaiaAllowedClients;'}
         )
         It "Testing for cmdlet <cmdlet>" -ForEach $cmdLets {
